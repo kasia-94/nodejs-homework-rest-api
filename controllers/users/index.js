2c45009c76fbed7075813890185e155ecc185c94
Kateryna Berehovych <kasia-94@ukr.net>
2023-01-28 13:05:56 +0200

    Add endpoint AVATARS

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -3,5 +3,13 @@ const loginUser = require("./loginUser");
 const current = require("./current");
 const logOut = require("./logOut");
 const updateSubscription = require("./updateSubscription");
+const updateAvatars = require("./updateAvatars");
 
-module.exports = { register, loginUser, current, logOut, updateSubscription };
+module.exports = {
+  register,
+  loginUser,
+  current,
+  logOut,
+  updateSubscription,
+  updateAvatars,
+};
--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { users: ctrl } = require("../../controllers");
 const auth = require("../../middlewares/auth");
+const { upload } = require("../../middlewares/storage");
 
 router.post("/register", ctrl.register);
 router.post("/login", ctrl.loginUser);
 router.get("/current", auth, ctrl.current);
 router.post("/logout", auth, ctrl.logOut);
 router.patch("/subscription", auth, ctrl.updateSubscription);
+router.patch("/avatars", auth, upload.single("avatar"), ctrl.updateAvatars);
 
 module.exports = router;
